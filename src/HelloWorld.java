be9506a505b412c032789866730de0c12aea0122
Flonly <1165377737@qq.com>
2020-04-26 15:22:59 +0800

    second test

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,5 +3,6 @@ public class HelloWorld {
         System.out.println("Hello World.");
         System.out.println("I am the change.");
         System.out.println("This will push on develop.");
+        System.out.println("This is the second change.");
     }
 }
